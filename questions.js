0f15abf245281ce691264f168242301a8d78e9fc
niyazmohammedniyaz0-collab <niyazmohammedniyaz0@gmail.com>
2026-01-16 12:09:46 +0530

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,19 @@
+const questions = [
+  {
+    question: "What does AWS stand for?",
+    options: ["Amazon Web Services", "Advanced Web Server", "Amazon Website System"],
+    answer: 1
+  },
+  {
+    question: "Which language is used in Node.js?",
+    options: ["Python", "Java", "JavaScript"],
+    answer: 3
+  },
+  {
+    question: "Which command initializes a Node.js project?",
+    options: ["node init", "npm init", "git init"],
+    answer: 2
+  }
+];
+
+module.exports = questions;
